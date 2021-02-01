4351864c361872782d3c416ca0a89f810f7bd958
ordinary_zahid <mjhasan38@gmail.com>
2021-02-01 10:40:50 +0600

    Add more intities and Seed data in Database

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -4,6 +4,12 @@ namespace Core.Entities
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
         
     }
 }
