c4f0e8af8e2ebfbdaf24167c02e97129721a46aa
ordinary_zahid <mjhasan38@gmail.com>
2021-02-07 10:00:23 +0600

    Adding nav-bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {IProduct} from './models/product';
+import {IPagination} from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +10,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'MultiShop';
-
+  products: IProduct[];
   constructor(private http: HttpClient) { }
 
    ngOnInit(): void {
-     this.http.get('https://localhost:5001/api/products').subscribe((response: any) => {
-        console.log(response);
+     this.http.get('https://localhost:5001/api/products').subscribe((response: IPagination) => {
+        this.products = response.data;
 
      }, error => {
        console.log(error);
      });
   }
 }
+
